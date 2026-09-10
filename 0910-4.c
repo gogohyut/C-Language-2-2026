7af77ed38a857dc53bc4cd6b12b34db0b58087dc
Choi Sang Hyeok <cch130223@gmail.com>
2026-09-10 11:36:14 +0900

    Call-by-value, 반환값 할당, 후위/전위 증감 연산자의 동작

---
+++ 0910-4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void print(int n);
+int increment(int n);
+
+int main() {
+    int n;
+    increment(n);
+    print(n);
+    n = increment(n);
+    print(n);
+    print(n++);
+    print(n);
+    return 0;
+}
+void print(int n) {
+    printf("%d\n", n);
+}
+int increment(int n) {
+    return ++n;
+}
